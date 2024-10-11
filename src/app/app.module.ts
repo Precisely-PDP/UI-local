e00f9b27b89e855eaff12e86915caa954d4fff19
maksymilian.sredniawa <maksymilian.sredniawa@precisely.com>
2024-10-11 14:54:03 +0200

    Added navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
+import {AppRoutingModule} from './app-routing.module';
 import {FormsModule} from '@angular/forms';
 import {SocketIoModule, SocketIoConfig} from 'ngx-socket-io';
 
@@ -8,6 +9,8 @@ import {TerminalComponent} from './components/terminal/terminal.component';
 import {TerminalListComponent} from './components/terminal-list/terminal-list.component';
 import {NgTerminalModule} from 'ng-terminal';
 import {TerminalsManagerComponent} from './components/TerminalsManager/terminals-manager.component';
+import {ConfigPageComponent} from './components/config-page/config-page.component';
+import {RouterLink, RouterOutlet} from '@angular/router';
 
 const config: SocketIoConfig = {url: 'http://localhost:4444', options: {}};
 
@@ -16,13 +19,17 @@ const config: SocketIoConfig = {url: 'http://localhost:4444', options: {}};
     AppComponent,
     TerminalComponent,
     TerminalListComponent,
-    TerminalsManagerComponent
+    TerminalsManagerComponent,
+    ConfigPageComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    AppRoutingModule,
     SocketIoModule.forRoot(config),
-    NgTerminalModule
+    NgTerminalModule,
+    RouterOutlet,
+    RouterLink
   ],
   providers: [],
   bootstrap: [AppComponent],
